772f4ac8038da9683607a78b3b7eb0d1d19cd939
Elena Cherpakova <elena.cherpakova@gmail.com>
2025-03-05 20:54:49 -0500

    Update solution - CodeHub

--- 7-kyu/vowel-count/vowel-count.py
+++ 7-kyu/vowel-count/vowel-count.py
@@ -1,4 +1,4 @@
 def get_count(sentence):
     vowels = 'aeiou'
-    return 0 if not sentence else sum(1 for char in sentence if char in vowels)
+    return sum(1 for char in sentence if char in vowels)
 ​
