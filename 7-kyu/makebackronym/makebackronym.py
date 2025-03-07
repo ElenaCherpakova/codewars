7fc87231aa14e6088eae3e1abdabef408c2a6246
Elena Cherpakova <elena.cherpakova@gmail.com>
2025-03-06 21:47:29 -0500

    Update solution - CodeHub

--- 7-kyu/makebackronym/makebackronym.py
+++ 7-kyu/makebackronym/makebackronym.py
@@ -1,12 +1,5 @@
-#preloaded variable: "dictionary"
+​
+from preloaded import dictionary
 ​
 def make_backronym(acronym):
-    backron_dict = {
-        'A': 'awesome', 'B': 'beautiful', 'C': 'confident', 'D': 'disturbing', 'E': 'eager', 
-        'F': 'fantastic', 'G': 'gregarious', 'H': 'hippy', 'I': 'ingestable', 'J': 'joke', 
-        'K': 'klingon', 'L': 'literal', 'M': 'mustache', 'N': 'newtonian', 'O': 'oscillating', 
-        'P': 'perfect', 'Q': 'queen', 'R': 'rant', 'S': 'stylish', 'T': 'turn', 
-        'U': 'underlying', 'V': 'volcano', 'W': 'weird', 'X': 'xylophone', 'Y': 'yogic', 
-        'Z': 'zero'
-    }
-    return ' '.join(backron_dict[char.upper()] for char in acronym)
+    return ' '.join(dictionary[char.upper()] for char in acronym)
