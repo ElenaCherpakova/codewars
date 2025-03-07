cc6ca812ec67f1dbe709c921009bdb6dfd90459d
Elena Cherpakova <elena.cherpakova@gmail.com>
2025-03-06 23:13:01 -0500

    Update transpose-two-strings-in-an-array.py

--- 7-kyu/transpose-two-strings-in-an-array/transpose-two-strings-in-an-array.py
+++ 7-kyu/transpose-two-strings-in-an-array/transpose-two-strings-in-an-array.py
@@ -1,13 +1,22 @@
+from itertools import zip_longest 
 def transpose_two_strings(arr):
     a, b = arr[0], arr[1]
+    t = zip_longest(a, b, fillvalue= ' ')
+    # for pair in t:
+    #     res.append(' '.join(pair))
+    # return '\n'.join(res)
+    return '\n'.join(' '.join(pair) for pair in t)
     
-    max_len = max(len(a), len(b))
-    a = a.ljust(max_len)
-    b = b.ljust(max_len)
     
-    res = []
-    t = zip(a, b)
+    # max_len = max(len(a), len(b))
+    # a = a.ljust(max_len)
+    # b = b.ljust(max_len)
     
-    for pair in t:
-        res.append(' '.join(pair))
-    return '\n'.join(res)
+    # res = []
+    # t = zip(a, b)
+    
+    # for pair in t:
+    #     res.append(' '.join(pair))
+    # return '\n'.join(res)
+
+    
