fe46f33addd8eb7367ae9cf3724ad41c7ec2169d
Elena Cherpakova <elena.cherpakova@gmail.com>
2025-03-15 19:15:17 -0400

    Update largest-elements.py

--- 7-kyu/largest-elements/largest-elements.py
+++ 7-kyu/largest-elements/largest-elements.py
@@ -1,5 +1,8 @@
 import heapq
 def largest(n, xs):
+    # O(m log m)
 #     sorted_list = sorted(xs)
 #     return sorted_list[-n:]
+
+    # O(m log n) - faster
     return sorted(heapq.nlargest(n, xs))
