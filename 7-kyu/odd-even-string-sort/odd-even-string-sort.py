92079d7f9681330a277aeeb53620d46045324abc
Elena Cherpakova <elena.cherpakova@gmail.com>
2025-03-16 14:23:47 -0400

    Update odd-even-string-sort.py

--- 7-kyu/odd-even-string-sort/odd-even-string-sort.py
+++ 7-kyu/odd-even-string-sort/odd-even-string-sort.py
@@ -1,8 +1,9 @@
 def sort_my_string(s):
-    even, odd = '', ''
-    for i, letter in enumerate(s):
-        if i % 2 == 0:
-            even +=letter
-        else:
-            odd += letter
-    return f'{even} {odd}'
+    # even, odd = '', ''
+    # for i, letter in enumerate(s):
+    #     if i % 2 == 0:
+    #         even +=letter
+    #     else:
+    #         odd += letter
+    # return f'{even} {odd}'
+    return f"{s[::2]} {s[1::2]}"
