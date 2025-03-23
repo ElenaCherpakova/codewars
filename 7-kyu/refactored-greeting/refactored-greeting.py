0cd75a186f22bee21d353683b7d20e2612f9ad7d
Elena Cherpakova <elena.cherpakova@gmail.com>
2025-03-23 18:44:54 -0400

    Update solution - CodeHub

--- 7-kyu/refactored-greeting/refactored-greeting.py
+++ 7-kyu/refactored-greeting/refactored-greeting.py
@@ -4,5 +4,5 @@ class Person:
     def __init__(self, name):
         self.name = name
         
-    def greet(self, name):
-        return "Hello %s, my name is %s" % (name, self.name)
+    def greet(self, your_name):
+        return "Hello %s, my name is %s" % (your_name, self.name)
