076e39c9c4fd6d591604f41d097267292712c3ae
Elena Cherpakova <elena.cherpakova@gmail.com>
2025-04-09 12:54:53 -0400

    Update emotional-sort-<.py

--- 6-kyu/emotional-sort-</emotional-sort-<.py
+++ 6-kyu/emotional-sort-</emotional-sort-<.py
@@ -1,4 +1,3 @@
 def sort_emotions(arr, order):
     emotions = {':D': 1, ':)': 2, ':|': 3, ':(': 4, 'T_T': 5}
-    sorted_arr = sorted(arr, key=lambda x: emotions[x], reverse=not order)
-    return sorted_arr
+    return sorted(arr, key=lambda x: emotions[x], reverse=not order)
