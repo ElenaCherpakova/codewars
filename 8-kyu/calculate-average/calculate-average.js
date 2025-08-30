d0808532ffe9a5960bd0e1f224ebc2d2a33f7ff4
Elena Cherpakova <elena.cherpakova@gmail.com>
2025-08-30 19:24:41 +0300

    Update calculate-average.js

--- 8-kyu/calculate-average/calculate-average.js
+++ 8-kyu/calculate-average/calculate-average.js
@@ -1,4 +1,4 @@
 function findAverage(array) {
   if(array.length === 0) return 0
-  return array.reduce((acc, count)=> acc + count / array.length, 0)
+  return array.reduce((acc, count)=> acc + count, 0) / array.length
 }
